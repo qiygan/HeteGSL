Nan gradient bug fixed (caused by dropout before embedding detach)

src/models/IDGL/IDGL.py
@@ -8,11 +8,10 @@ class IDGL(nn.Module):
 
     def __init__(self, args, nfeat, nclass):
         super(IDGL, self).__init__()
-        self.GCN = GCN_with_emb(nfeat, args.num_hidden, nclass, dropout=0.5)
+        self.GCN = GCN_with_emb(nfeat, args.num_hidden, nclass, dropout=args.dropout)
         self.GenAdjLayer = IDGL_AdjGenerator(nfeat, args.num_hidden, args.num_head, args.epsilon)
         self.lambda_ = args.lambda_
         self.eta = args.eta
-
 
     def forward(self, x, h, adj, adj_feat, mode):
         """
@@ -59,6 +58,7 @@ class GCN_with_emb(nn.Module):
         if self.normalize:
             x = F.normalize(x, p=2, dim=1)
         x = F.relu(x)
+        emb = x.detach()
         x = F.dropout(x, self.dropout, training=self.training)
         # Classification Layer
         x2 = self.gc2(x, adj)
@@ -66,5 +66,5 @@ class GCN_with_emb(nn.Module):
             x2 = F.normalize(x2, p=2, dim=1)
 
         x2 = F.relu(x2)
-        return F.log_softmax(x2, dim=1), x.detach()
+        return F.log_softmax(x2, dim=1), emb
         # ! Note that the detach() operation is vital, 下一个循环中我们要根据adj update的是metric向量而不是网络参数

src/models/IDGL/layers.py
@@ -34,6 +34,7 @@ class IDGL_AdjGenerator(nn.Module):
     def normalize_adj_torch(self, adj, mode='sc'):
         """Row-normalize sparse matrix"""
         rowsum = torch.sum(adj, 1)
+        # r_inv_sqrt = torch.pow(rowsum, -0.5).flatten()  # Abandoned, gen nan for zero values.
         r_inv_sqrt = torch.pow(rowsum + 1e-8, -0.5).flatten()
         r_inv_sqrt[torch.isinf(r_inv_sqrt)] = 0.
         r_mat_inv_sqrt = torch.diag(r_inv_sqrt)
@@ -51,36 +52,21 @@ class IDGL_AdjGenerator(nn.Module):
         """
         # TODO Zero mat, necessary?
         s = torch.zeros((h.shape[0], h.shape[0])).cuda()
+        zero_lines = torch.where(torch.sum(h, 1) == 0)[0]
+        if len(zero_lines) > 0:
+            raise ValueError('{} zero lines in {}s!\nZero lines:{}'.format(len(zero_lines), mode, zero_lines))
         for i in range(self.num_head):
             if mode == 'feat':  # First time, use feat as emb.
-                h_prime = self.metric_layer_feat[i](h)
+                weighted_h = self.metric_layer_feat[i](h)
             elif mode == 'emb':
-                h_prime = self.metric_layer_emb[i](h)
-            if torch.isnan(h_prime[1, 1]):
-                print()
-            s += cos_sim(h_prime, h_prime)
-
+                weighted_h = self.metric_layer_emb[i](h)
+            s += cos_sim(weighted_h, weighted_h)
         s /= self.num_head
         # Remove negative values (Otherwise Nans are generated for negative values with power operation
         s = torch.where(s < self.threshold, torch.zeros_like(s), s)
-        # check whether all positive:  sum(sum(s >= 0)).item() == 7333264
         # s = self.normalize_adj_torch(s)
         s = F.normalize(s, dim=1, p=1)  # Row normalization
         return s
-
-
-# ! Previous version
-#         s = torch.zeros((h.shape[0], h.shape[0])).cuda()
-#         for i in range(self.num_head):
-#             h_prime = self.metric_layer[i](h)
-#             s += cos_sim(h_prime, h_prime)
-#         s /= self.num_head
-#         rst = torch.where(s < self.threshold, torch.zeros_like(s), s)
-#         rst = F.normalize(rst, dim=1, p=1)  # Row normalization
-#         return (1 - self.lambda_) * rst + self.lambda_adj
-
-
-# pylint: disable= no-member, arguments-differ, invalid-name
 
 import torch
 import torch.nn as nn
@@ -109,6 +95,9 @@ class GraphConvolution(nn.Module):  # GCN AHW
     def forward(self, inputs, adj):
         support = torch.spmm(inputs, self.weight)  # HW in GCN
         output = torch.spmm(adj, support)  # AHW
+        zero_lines = torch.where(torch.sum(output, 1) == 0)[0]
+        if len(zero_lines) > 0:
+            print('{} zero lines in embeddings!\nZero lines:{}'.format(len(zero_lines), zero_lines))
         if self.bias is not None:
             return output + self.bias
         else:

src/models/IDGL/train.py
@@ -1,5 +1,6 @@
 import sys
 import os
+
 cur_path = os.path.abspath(os.path.dirname(__file__))
 root_path = cur_path.split('/models')[0]
 sys.path.append(root_path)
@@ -9,7 +10,7 @@ from utils.util_funcs import *
 # shell_init(server='S5', gpu_id=3)
 # shell_init(server='S5', gpu_id=2)
 # shell_init(server='S5', gpu_id=4, f_prefix='src/models/IDGL')
-shell_init(server='Ali', gpu_id=3, f_prefix='src/models/IDGL')
+shell_init(server='Ali', gpu_id=3)
 import argparse
 import networkx as nx
 import time
@@ -33,7 +34,18 @@ def normalize_adj_torch(adj):
 
 
 def graph_reg_loss(args, adj, features):
-    return torch.Tensor(0).cuda()
+    # Dirichlet energy
+    degree_mat = torch.diag(torch.sum(adj, 1))
+    laplacian_mat = adj - degree_mat
+    _ = torch.mm(features.T, laplacian_mat)
+    _ = torch.mm(_, features)
+    dir_energy = torch.trace(_)
+    # f(A)
+    degree_vec = torch.sum(adj, 1)
+    connectivity = -1 * torch.sum(torch.log(degree_vec))
+    sparsity = torch.norm(adj, p=2)  # torch.isinf(sparsity)
+    res = args.alpha * dir_energy + args.beta * connectivity + args.gamma * sparsity
+    return res
 
 
 def accuracy(logits, labels):
@@ -60,12 +72,13 @@ def iter_condition(args, adj_prev, adj_new, ori_adj_norm, t):
     return cond1 and cond2
 
 
-def cal_loss(args, cla_loss, logits, train_mask, labels, adj, features):
+def cal_loss(args, cla_loss, logits, train_mask, labels, adj=None, features=None, mode='default'):
     l_pred = cla_loss(logits[train_mask], labels[train_mask])
-    # l_graph = graph_reg_loss(args, adj, features)
-    # loss = l_pred + l_graph
-    # return loss
-    return l_pred  # Fixme
+    if mode == 'pretrain':
+        return l_pred  # Fixme
+    l_graph = graph_reg_loss(args, adj, features)
+    loss = l_pred + l_graph
+    return loss
 
 
 # load and preprocess dataset
@@ -132,6 +145,19 @@ def train_idgl(args):
     adj = g.adjacency_matrix().cuda()
     adj = normalize_adj_torch(adj.to_dense())
     ori_adj_norm = torch.norm(adj, p=2)
+    # ! Pretrain
+    for i in range(args.pretrain):
+        logits, h, adj_sim, adj_feat = model(features, h=None, adj=adj, adj_feat=None, mode='feat')
+        loss = cal_loss(args, cla_loss, logits, train_mask, labels, mode='pretrain')
+        optimizer.zero_grad()
+        # Stops if get annomaly
+        with torch.autograd.detect_anomaly():
+            loss.backward()
+        optimizer.step()
+        test_acc = evaluate(model, features, labels, test_mask, adj)
+        print("Pretrain Test Accuracy {:.4f}".format(test_acc))
+    print("Pretrain Finished!\n")
+    # ! Train
     for epoch in range(args.epochs):
         model.train()
         if epoch >= 3:
@@ -147,17 +173,15 @@ def train_idgl(args):
             adj_sim_prev = adj_sim.detach()
             logits, h, adj_sim, adj_agg = model(features, h, adj, adj_feat, mode='emb')
             loss_adj_emb = cal_loss(args, cla_loss, logits, train_mask, labels, adj_sim, features)
+            zero_lines = torch.where(torch.sum(h, 1) == 0)[0]
             loss_list.append(loss_adj_emb)
         loss = torch.mean(torch.stack(loss_list))
         optimizer.zero_grad()
 
-        # 反向传播时：在求导时开启侦测
+        # Stops if get annomaly
         with torch.autograd.detect_anomaly():
             loss.backward()
-        # print(model.GenAdjLayer.metric_layer_emb[0].weight) # normal
         optimizer.step()
-        # print(model.GenAdjLayer.metric_layer_emb[0].weight) # ! NAN
-        # print(model.GenAdjLayer.metric_layer_feat[0].weight) # ! NAN
 
         if epoch >= 3:
             dur.append(time.time() - t0)
@@ -165,55 +189,64 @@ def train_idgl(args):
         train_acc = accuracy(logits[train_mask], labels[train_mask])
 
         val_acc = evaluate(model, features, labels, val_mask, adj)
-
+        test_acc = evaluate(model, features, labels, test_mask, adj)
+        # print("Epoch {:05d} | Time(s) {:.4f} | Loss {:.4f} | TrainAcc {:.4f} |"
+        #       " ValAcc {:.4f} | ETputs(KTEPS) {:.2f}".
+        #       format(epoch, np.mean(dur), loss.item(), train_acc,
+        #              val_acc, n_edges / np.mean(dur) / 1000))
         print("Epoch {:05d} | Time(s) {:.4f} | Loss {:.4f} | TrainAcc {:.4f} |"
-              " ValAcc {:.4f} | ETputs(KTEPS) {:.2f}".
+              " ValAcc {:.4f} | TestAcc {:.4f}".
               format(epoch, np.mean(dur), loss.item(), train_acc,
-                     val_acc, n_edges / np.mean(dur) / 1000))
+                     val_acc, test_acc))
+        # if epoch % 100 == 0:
+        #     test_acc = evaluate(model, features, labels, test_mask, adj)
+        #     print("Test Accuracy {:.4f}".format(test_acc))
 
-    print()
-    acc = evaluate(model, features, labels, test_mask, adj)
-    print("Test Accuracy {:.4f}".format(acc))
-    res_dict = {'parameters': args.__dict__, 'res': {'acc': '{:.4f}'.format(acc)}}
+    test_acc = evaluate(model, features, labels, test_mask, adj)
+    print("Test Accuracy {:.4f}".format(test_acc))
+    res_dict = {'parameters': args.__dict__, 'res': {'acc': '{:.4f}'.format(test_acc)}}
     return res_dict
 
 
 if __name__ == '__main__':
+    # ! Train Configs
     parser = argparse.ArgumentParser(description='GAT_dgl')
     parser.add_argument("--gpu", type=int, default=0,
                         help="which GPU to use. Set -1 to use CPU.")
     parser.add_argument("--dataset", type=str, default='cora',
                         help="dataset to use")
-    parser.add_argument("--num_hidden", type=int, default=8,
-                        help="number of hidden units")
-    parser.add_argument("--epochs", type=int, default=300,
-                        help="number of training epochs")
-    parser.add_argument("--seed", type=int, default=0,
-                        help="training seed")
-    parser.add_argument('--weight_decay', type=float, default=5e-4,
-                        help="weight decay")  # Fixme
-    # Model configs
-    parser.add_argument("--lambda_", type=float, default=0.9,
-                        help="ratio of retain the original graph")
-    parser.add_argument("--eta", type=float, default=0.0)
-    parser.add_argument("--alpha", type=float, default=0.2)
-    parser.add_argument("--beta", type=float, default=0.0)
-    parser.add_argument("--gamma", type=float, default=0.0)
-    parser.add_argument("--epsilon", type=float, default=0.0)
-    parser.add_argument("--num_head", type=int, default=3,
-                        help="number of metric heads, m in paper")
-    parser.add_argument("--delta", type=float, default=4e-5)
-    parser.add_argument("--T", type=int, default=10)
-    parser.add_argument("--lr", type=float, default=1e-3, help="learning rate")
     parser.add_argument('--out_path', type=str, default='results/IDGL/',
                         help="path of results")
     parser.add_argument('--exp_name', type=str, default='IDGL_Results.txt',
                         help="name of the experiment")
-    args = parser.parse_args()
+    parser.add_argument("--epochs", type=int, default=300,
+                        help="number of training epochs")
+    parser.add_argument("--num_hidden", type=int, default=16,
+                        help="number of hidden units")
+    parser.add_argument("--seed", type=int, default=2020,
+                        help="training seed")
+    parser.add_argument('--weight_decay', type=float, default=5e-4,
+                        help="weight decay")  # Fixme
+    # ! Original IDGL Configs
+    parser.add_argument("--lambda_", type=float, default=0.9,
+                        help="ratio of retain the original graph")
+    parser.add_argument("--eta", type=float, default=0.1)
+    parser.add_argument("--alpha", type=float, default=0.2)
+    parser.add_argument("--beta", type=float, default=0.0)
+    parser.add_argument("--gamma", type=float, default=0.0)
+    parser.add_argument("--epsilon", type=float, default=0.0)
+    parser.add_argument("--num_head", type=int, default=4,
+                        help="number of metric heads, m in paper")
+    parser.add_argument("--delta", type=float, default=4e-5)
+    parser.add_argument("--T", type=int, default=10)
+    parser.add_argument("--lr", type=float, default=1e-2, help="learning rate")
+    parser.add_argument("--dropout", type=float, default=0.5, help="dropout rate")
+    # ! Added Model configs
+    parser.add_argument("--pretrain", type=int, default=100, help="pretrain epochs")
 
-    # !  Debug Target: No graph reg_loss training, 0.8020
-    args.T = 0 # Fixme
-    args.lambda_= 1
+    args = parser.parse_args()
+    args.epochs = 200
+    args.pretrain = 100
     print(args)
     res_dict = train_idgl(args)
     write_dict(res_dict, args.out_path + args.exp_name)